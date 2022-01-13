aed10054644cbfffb3b45292611b3ad8123e9b15
Jhasterina <85872607+Jhasterina@users.noreply.github.com>
2022-01-13 15:31:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ function preload()
 rightWristX = 0;
 rightWristY = 0;
 
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
 leftWristX = 0;
 leftWristY = 0;
 
@@ -47,6 +50,38 @@ function gotPoses(results)
 
 function draw() {
 	image(video, 0, 0, 600, 500);
+    fill('#FF0000');
+    stroke('#FF0000');
+
+	if (scoreRightWrist > 0.2) {
+		circle(rightWristX, rightWristY, 20);
+		if (rightWristY > 0 && rightWristY <= 100)
+		{
+			document.getElementById("speed").innerHTML = "speed = 0.5x";
+			song.rate(0.5);
+		} else if (rightWristY > 100 && rightWristY <= 200) {
+			document.getElementById("speed").innerHTML = "speed = 1x";
+			song.rate(1);
+		} else if (rightWristY > 200 && rightWristY <= 300){
+			document.getElementById("speed").innerHTML = "speed = 1.5x";
+			song.rate(1.5);
+		} else if (rightWristY > 300 && rightWristY <= 400) {
+			document.getElementById("speed").innerHTML = "speed = 2x";
+			song.rate(2);
+		} else if (rightWristY > 400 && rightWristY <= 500) {
+			document.getElementById("speed").innerHTML = "speed = 2.5x";
+			song.rate(2.5);
+		}
+	}
+
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        inNumberleftWristY = Number(leftWristY);
+        removeDecimals = floor(inNumberleftWristY);
+        volume = removeDecimals/500;
+        document.getElementById('volume').innerHTML = "volume =" + volume;
+        song.setVolume(volume);
+    }
 }
 
 function play()
